f86f666a5bf028a5739ec8b7fe69f942f31c3dd1
YogeshMaan <170030012@iitdh.ac.in>
2023-09-16 18:50:27 -0700

    Pull experiment setup working

--- xplorer_offboard_control/pulley_pull.py
+++ xplorer_offboard_control/pulley_pull.py
@@ -29,10 +29,13 @@ class PulleyPull(Node):
         self.offboard_setpoint_counter = 0
         self.vehicle_odometry = VehicleOdometry()
         self.vehicle_status = VehicleStatus()
-        self.waypoints = self.generate_waypoints()
+        self.waypoints = []
         self.wp_num = 0
         self.err = 10 #initialise as high value
         self.thres_err = .15
+        self.initial_pose = VehicleOdometry()
+        self.t_initial = self.get_clock().now().nanoseconds / 10**9
+        self.thres_delta_t = 30
 
 
 
@@ -44,7 +47,10 @@ class PulleyPull(Node):
         self.get_logger().info("----Generating Waypoints----")
         wp = []
 
-        wp.append([-0.5,0.0,-.75, 0.0 , 0.0,float("nan")])
+        wp.append([self.initial_pose.x, self.initial_pose.y, self.initial_pose.z-.30]) # wp_num = 0
+        wp.append([self.initial_pose.x, self.initial_pose.y+.70, self.initial_pose.z-.30 ]) # wp_num = 1
+        wp.append([self.initial_pose.x, self.initial_pose.y, self.initial_pose.z-.30]) # wp_num = 2
+        wp.append([self.initial_pose.x, self.initial_pose.y, self.initial_pose.z ]) # wp_num = 3
         
         self.get_logger().info("----Waypoint generation completed!----")
         return wp
@@ -115,24 +121,37 @@ class PulleyPull(Node):
     def timer_callback(self) -> None:
         self.publish_offboard_control_heartbeat_signal()
 
+        #Mission Timer
+        delta_t = self.get_clock().now().nanoseconds/10**9 - self.t_initial
+
+        # generate waypoints w.r.t current position
+        if self.offboard_setpoint_counter == 9:
+            self.initial_pose = self.vehicle_odometry
+            self.waypoints = self.generate_waypoints()
+            print(self.waypoints)
+
         if self.offboard_setpoint_counter == 10:
             self.engage_offboard_mode()
             self.arm()
 
         if self.offboard_setpoint_counter < 11:
-            self.offboard_setpoint_counter +=1        
+            self.offboard_setpoint_counter +=1  
+
+        # To hold on to the position at wp_num==1
+        if delta_t < self.thres_delta_t and self.wp_num == 1: 
+            self.err = self.thres_err + 1.0      
 
         # Thres_err based waypoint follower
         if self.err > self.thres_err and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.publish_position_setpoint(self.waypoints[self.wp_num][0], self.waypoints[self.wp_num][1], self.waypoints[self.wp_num][2], self.waypoints[self.wp_num][3], self.waypoints[self.wp_num][4], self.waypoints[self.wp_num][5] )
+            self.publish_position_setpoint(self.waypoints[self.wp_num][0], self.waypoints[self.wp_num][1], self.waypoints[self.wp_num][2])
 
         elif self.err <= self.thres_err and self.wp_num < len(self.waypoints) and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.publish_position_setpoint(self.waypoints[self.wp_num][0], self.waypoints[self.wp_num][1], self.waypoints[self.wp_num][2], self.waypoints[self.wp_num][3], self.waypoints[self.wp_num][4], self.waypoints[self.wp_num][5])
+            self.publish_position_setpoint(self.waypoints[self.wp_num][0], self.waypoints[self.wp_num][1], self.waypoints[self.wp_num][2])
             self.wp_num +=1
 
         # Thres_err based waypoint follower: Landing setpoint after reaching the last waypoint
         elif self.err <= self.thres_err and self.wp_num >= len(self.waypoints):
-            self.publish_position_setpoint(0.0, 0.0, 0.0, float("nan"),float("nan"),float("nan"))     
+            self.publish_position_setpoint(self.initial_pose.x, self.initial_pose.y, self.initial_pose.z )     
             exit(0)
 
         # Compute error using des_setpoint - curr_setpoint
@@ -141,6 +160,7 @@ class PulleyPull(Node):
     
 
         self.get_logger().info(f"Error: {self.err}")
+        self.get_logger().info(f"Mission Time (sec): {delta_t}")
 
 
 def main(args = None) -> None:
